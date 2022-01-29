2fc38e1e7ee1267682afa67559c790f818bfd3a8
Sujan032 <sujandebnath032@gmail.com>
2022-01-29 13:06:04 +0530

    version 3

--- index.js
+++ index.js
@@ -26,33 +26,33 @@ function makeSound(key) {
 
 
     switch (key) {
-        case "w":
+        case "z":
             var crash = new Audio("sounds/crash.mp3");
             crash.play();
             break;
 
-        case "a":
+        case "x":
             var kick = new Audio("sounds/kick.mp3");
             kick.play();
             break;
 
-        case "s":
+        case "c":
             var snare = new Audio("sounds/snare.mp3");
             snare.play();
             break;
-        case "d":
+        case "v":
             var tom1 = new Audio("sounds/tom-1.mp3");
             tom1.play();
             break;
-        case "j":
+        case "b":
             var tom2 = new Audio("sounds/tom-2.mp3");
             tom2.play();
             break;
-        case "k":
+        case "n":
             var tom3 = new Audio("sounds/tom-3.mp3");
             tom3.play();
             break;
-        case "l":
+        case "m":
             var tom4 = new Audio("sounds/tom-4.mp3");
             tom4.play();
             break;
